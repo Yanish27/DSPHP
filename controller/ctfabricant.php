4a8b7e97977804b850a340cc0eb5fb8e8e1f5e66
SIO2 <yanis.halaoui@yanish.fr>
2021-11-22 14:24:32 +0100

    ctFabricant correction

--- controller/ctfabricant.php
+++ controller/ctfabricant.php
@@ -3,9 +3,9 @@
 public function ListeVueVolbyFabricant($Fabricant)
 {
     $Fab = new FabricantDAO();
-    $GetListeVueVolbyFabricant = $Modele->GetVueVolByFab($Fabricant);
+    $GetListeVueVolbyFabricant = $Fab->GetVueVolByFab($Fabricant);
 
     $DataFab1 = GetListeVueVolbyFabricant->fetch(PDO::FETCH_ASSOC);
     var_dump($DataFab1);
-    
+
 }
